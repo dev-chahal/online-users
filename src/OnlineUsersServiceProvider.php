0b805fef412055ec3562ea6f0131d3596fa60827
Sahil Chahal <dev.sahilchahal@gmail.com>
2022-08-12 16:44:14 +0530

    Registered user migration

--- src/OnlineUsersServiceProvider.php
+++ src/OnlineUsersServiceProvider.php
@@ -15,15 +15,23 @@ class OnlineUsersServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        /* Registering the Console Commands */
         if ($this->app->runningInConsole()) {
+            /* Registering the Console Commands */
             $this->commands([
                 InstallOnlineUsers::class,
             ]);
 
+            /* Registering the Config File */
             $this->publishes([
                 __DIR__.'/../config/onlineusers.php' => config_path('onlineusers.php'),
             ], 'config');
+
+            /* Registering the Migrations */
+            if (! class_exists('UpdateUserTable')) {
+                $this->publishes([
+                    __DIR__ . '/../database/migrations/add_last_seen_users.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_add_last_seen_users.php'),
+                ], 'migrations');
+            }
         }
     }
 }
